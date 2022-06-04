4c3739de2d88a547fc342767fa904d1ab44ce7e1
Alex Malkevich <malkevich.alex@gmail.com>
2022-06-04 03:10:09 +0200

    test(integration): fix integration tests

--- integration/lerna-init.test.js
+++ integration/lerna-init.test.js
@@ -29,7 +29,7 @@ describe("lerna init", () => {
     expect(packageJson).toMatchInlineSnapshot(`
       Object {
         "devDependencies": Object {
-          "@spryker-lerna/lerna": "^__TEST_VERSION__",
+          "lerna": "^__TEST_VERSION__",
         },
         "name": "root",
         "private": true,
@@ -63,7 +63,7 @@ describe("lerna init", () => {
     expect(packageJson).toMatchInlineSnapshot(`
       Object {
         "devDependencies": Object {
-          "@spryker-lerna/lerna": "__TEST_VERSION__",
+          "lerna": "__TEST_VERSION__",
         },
         "name": "updates",
       }
